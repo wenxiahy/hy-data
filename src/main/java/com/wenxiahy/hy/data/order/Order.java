3d9e7a646a7111775edcae75f4b69393bf281615
周伟 <zhouwei@techfuser.net>
2020-12-29 15:47:25 +0800

    first commit

---
+++ src/main/java/com/wenxiahy/hy/data/order/Order.java
@@ -0,0 +1,78 @@
+package com.wenxiahy.hy.data.order;
+
+import java.util.Date;
+
+/**
+ * @Author zhouw
+ * @Description
+ * @Date 2020-12-29
+ */
+public class Order {
+
+    private Long orderNo;
+    private Long userId;
+
+    private int orderType;
+    private int orderStatus;
+
+    private String storeCode;
+    private String storeName;
+
+    private Date orderTime;
+
+    public Long getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Long orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public int getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(int orderType) {
+        this.orderType = orderType;
+    }
+
+    public int getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(int orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getStoreCode() {
+        return storeCode;
+    }
+
+    public void setStoreCode(String storeCode) {
+        this.storeCode = storeCode;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public Date getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Date orderTime) {
+        this.orderTime = orderTime;
+    }
+}
---
+++ src/main/java/com/wenxiahy/hy/data/user/User.java
@@ -0,0 +1,37 @@
+package com.wenxiahy.hy.data.user;
+
+/**
+ * @Author zhouw
+ * @Description
+ * @Date 2020-12-29
+ */
+public class User {
+
+    private Long userId;
+    private String username;
+    private String password;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
